9b7f4a961261007ae22134b8d0473e8e8d46c60b
Chris Amanse <chris@chrisamanse.xyz>
2016-09-23 18:00:12 +0800

    Add Codegen iOS target

---
+++ Codegen iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codegen iOS
+//
+//  Created by Chris Amanse on 09/23/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
