7bfb98a855773c34dfe6e9a199a2fbd837e8214b
Chris Amanse <chris@chrisamanse.xyz>
2016-09-27 18:00:27 +0800

    Fix setup Realm code and improve logging

--- Codegen iOS/AppDelegate.swift
+++ Codegen iOS/AppDelegate.swift
@@ -24,24 +24,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             // Inspect key
             guard let key = Realm.Configuration.defaultConfiguration.encryptionKey else {
-                fatalError("No key found!")
+                fatalError("No key found in default Realm.Configuration!")
             }
             
-            let hexString = key.map {
-                String(format: "%02x", $0)
-                }.joined()
-            
-            print("Key data: \(hexString)")
-            
-            // Try to create a Realm (initializes Realm files)
+            // Try to create a Realm (initializes Realm files if not yet initialized)
             let realm = try Realm()
             
-            // Ensure default Realm has key
+            // Ensure default Realm has the same key
             if realm.configuration.encryptionKey != key {
-                fatalError("Different keys used!")
+                fatalError("Unexpected key!")
+            }
+        } catch let error as RealmKeyError {
+            switch error {
+            case .failedToFetchStoredKey(status: let status):
+                fatalError("Failed to fetch stored key: \(status)")
+            case .failedToSaveGeneratedKey(status: let status):
+                fatalError("Failed to save generated key: \(status)")
+            case .failedToGenerateKey(error: let error):
+                fatalError("Failed to generate key: \(error)")
+            case .unexpectedResult(status: let status, result: let result):
+                fatalError("Unexpected fetch result: \(result)\n\nStatus: \(status)")
             }
         } catch let error {
-            fatalError("Failed to generate RealmKey!: \(error)")
+            fatalError("Failed to setup Realm: \(error)")
         }
         
         return true
