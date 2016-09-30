3fcde5ef44bd0b12f45de570696897dc63e2bd81
Chris Amanse <chris@chrisamanse.xyz>
2016-09-30 18:30:51 +0800

    Create AccountTableViewCell and connect to UI

---
+++ Codegen iOS/Views/AccountTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AccountTableViewCell.swift
+//  Codegen
+//
+//  Created by Chris Amanse on 09/30/2016.
+//
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var issuerLabel: UILabel!
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var accountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
