68441a14999db0b7aad3cc33e0e1cc25d6fe4f66
Chris Amanse <chris@chrisamanse.xyz>
2016-10-01 13:31:24 +0800

    Add delete account feature

--- Codegen iOS/ViewControllers/AccountsTableViewController.swift
+++ Codegen iOS/ViewControllers/AccountsTableViewController.swift
@@ -105,19 +105,22 @@ class AccountsTableViewController: UITableViewController {
         return true
     }
     */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            guard let account = accounts?[indexPath.row] else {
+                return
+            }
+            
+            try? realm?.write {
+                realm?.delete(account)
+            }
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
