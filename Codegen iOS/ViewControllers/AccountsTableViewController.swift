ed9db6f311efe19b4cadfb7390a4582ba5f9e495
Chris Amanse <chris@chrisamanse.xyz>
2016-11-30 17:59:30 -0800

    Remove unused override didReceiveMemoryWarning()

--- Codegen iOS/ViewControllers/AccountsTableViewController.swift
+++ Codegen iOS/ViewControllers/AccountsTableViewController.swift
@@ -65,11 +65,6 @@ class AccountsTableViewController: UITableViewController {
         destroyTimer()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     deinit {
         destroyTimer()
     }
