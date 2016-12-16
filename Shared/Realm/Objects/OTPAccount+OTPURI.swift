806e8db8e7e704ece0d64d2c6dc17a78523b5f8a
Chris Amanse <chris@chrisamanse.xyz>
2016-12-15 19:25:53 -0800

    Rename url parameter to uri in OTPAccount.init

--- Shared/Realm/Objects/OTPAccount+OTPURI.swift
+++ Shared/Realm/Objects/OTPAccount+OTPURI.swift
@@ -11,13 +11,13 @@ import CryptoKit
 import OTPKit
 
 extension OTPAccount {
-    convenience init?(url: OTPURI) {
+    convenience init?(uri: OTPURI) {
         self.init()
         
-        switch url.type.lowercased() {
+        switch uri.type.lowercased() {
         case OTPURI.Types.totp:
             // Period
-            if let value = url.parameters[OTPURI.Keys.period], let interval = TimeInterval(value) {
+            if let value = uri.parameters[OTPURI.Keys.period], let interval = TimeInterval(value) {
                 self.period = interval
             } else {
                 self.period = Defaults.period
@@ -26,7 +26,7 @@ extension OTPAccount {
             self.timeBased = true
         case OTPURI.Types.hotp:
             // Counter
-            guard let value = url.parameters[OTPURI.Keys.counter], let counter = UInt64(value) else {
+            guard let value = uri.parameters[OTPURI.Keys.counter], let counter = UInt64(value) else {
                 return nil
             }
             
@@ -38,37 +38,37 @@ extension OTPAccount {
         }
         
         // HashFunction
-        if let algorithm = url.parameters[OTPURI.Keys.algorithm]?.lowercased(), let hash = HashFunction(rawValue: algorithm) {
+        if let algorithm = uri.parameters[OTPURI.Keys.algorithm]?.lowercased(), let hash = HashFunction(rawValue: algorithm) {
             self.hashFunction = hash
         } else {
             self.hashFunction = Defaults.hashFunction
         }
         
         // Digits
-        if let value = url.parameters[OTPURI.Keys.digits], let intValue = Int(value) {
+        if let value = uri.parameters[OTPURI.Keys.digits], let intValue = Int(value) {
             self.digits = intValue
         } else {
             self.digits = Defaults.digits
         }
         
         // Issuer and Account
-        let labelComponents = url.label.components(separatedBy: ":")
+        let labelComponents = uri.label.components(separatedBy: ":")
         if labelComponents.count > 1 {
             // Has prefixed issuer
             self.issuer = labelComponents[0]
             self.account = labelComponents.dropFirst().joined(separator: ":")
         } else {
             // If no prefixed issuer, find it in parameters
-            if let issuer = url.parameters[OTPURI.Keys.issuer] {
+            if let issuer = uri.parameters[OTPURI.Keys.issuer] {
                 self.issuer = issuer
             }
             
             // Label is account
-            self.account = url.label
+            self.account = uri.label
         }
         
         // Key
-        guard let value = url.parameters[OTPURI.Keys.secret], let data = try? Base32.decode(value) else {
+        guard let value = uri.parameters[OTPURI.Keys.secret], let data = try? Base32.decode(value) else {
             return nil
         }
         
