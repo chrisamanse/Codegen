1a0a4e76dc74b57617d1439711880d82a2679229
Chris Amanse <chris@chrisamanse.xyz>
2016-12-15 17:18:05 -0800

    Disable rotation for CameraNavigationController
    - this disables scan for ScanViewController

---
+++ Codegen iOS/ViewControllers/CameraNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CameraNavigationController.swift
+//  Codegen
+//
+//  Created by Chris Amanse on 12/15/16.
+//
+//
+
+import UIKit
+
+class CameraNavigationController: UINavigationController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
