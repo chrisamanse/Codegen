91981369292ea26249951e0df8376a9e081704e0
Chris Amanse <chris@chrisamanse.xyz>
2016-12-29 18:17:27 -0800

    Create MoreViewController

---
+++ Codegen iOS/ViewControllers/MoreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MoreViewController.swift
+//  Codegen
+//
+//  Created by Chris Amanse on 12/29/16.
+//
+//
+
+import UIKit
+
+class MoreViewController: UITableViewController {
+    @IBAction func didPressDone(_ sender: AnyObject) {
+        dismiss(animated: true)
+    }
+}
