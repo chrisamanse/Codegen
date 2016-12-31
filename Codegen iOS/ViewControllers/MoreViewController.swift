943178067bf4b0d716af88ee3e5555faf3ddded5
Chris Amanse <chris@chrisamanse.xyz>
2016-12-30 17:38:52 -0800

    Add support link in MoreViewController

--- Codegen iOS/ViewControllers/MoreViewController.swift
+++ Codegen iOS/ViewControllers/MoreViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MoreViewController: UITableViewController {
+    private let supportURLString = "http://www.chrisamanse.xyz/"
+    
     @IBAction func didPressDone(_ sender: AnyObject) {
         dismiss(animated: true)
     }
@@ -16,6 +18,8 @@ class MoreViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             showLicenses()
+        } else if indexPath.row == 1 {
+            openSupport()
         }
     }
     
@@ -23,5 +27,13 @@ class MoreViewController: UITableViewController {
         let viewController = LicensesTableViewController(licenses: AppStrings.Licenses.all)
         
         navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func openSupport() {
+        guard let url = URL(string: supportURLString) else {
+            return
+        }
+        
+        UIApplication.shared.open(url)
     }
 }
